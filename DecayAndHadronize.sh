ae7ad6d047258cffd9e7e4d20eac07546c1c2a26
Andrea <massironi.andrea@gmail.com>
2013-09-17 10:15:29 +0200

    boh

--- DecayAndHadronize.sh
+++ DecayAndHadronize.sh
@@ -3,63 +3,103 @@
 
 NUMBERSEED=$1
 
-FOLDERNAME=$2
+CASTORFOLDERNAME=$2
 
 MODEL=$3
 
 ENERGY=$4
 
+CSI=$5
 
-NAMEFILELHE=$MODEL"_unweighted_events.lhe.gz"
 
-# before "higgs" transformation
-NAMELHE=$MODEL"_unweighted_events.lhe"
-# after "higgs" transformation
-NAMELHEMODIFIED=$MODEL"_"$NUMBERSEED"_"$FOLDERNAME"_unweighted_events_modified.lhe"
+
+# LHE before "higgs" transformation
+# fixed!
+
+# LHE after "higgs" transformation
+NAMELHEMODIFIED=$MODEL"_"$NUMBERSEED"_"$CSI"_"$ENERGY"_unweighted_events_modified.lhe"
 # EDM level file
-NAMEEDM=$MODEL"_"$NUMBERSEED"_"$FOLDERNAME"_EDM.root"
+NAMEEDM=$MODEL"_"$NUMBERSEED"_"$CSI"_"$ENERGY"_EDM.root"
 # GEN level file
-NAMEGEN=$MODEL"_"$NUMBERSEED"_"$FOLDERNAME"_GEN.root"
+NAMEGEN=$MODEL"_"$NUMBERSEED"_"$CSI"_"$ENERGY"_GEN.root"
 
 
-cp  $FOLDERNAME$NUMBERSEED/Events/$NAMEFILELHE /tmp/amassiro/
-cd /tmp/amassiro/
-gzip -d $NAMEFILELHE
-
-cd -
 
 
-### modify H -> 135, to make it properly decay
-awk '/\ \ 25\ \ /&&v++%2{sub(/\ \ 25\ \ \ \ 1/, "\ \ 35\ \ \ \ 1")}{print}' /tmp/amassiro/$NAMELHE  > /tmp/amassiro/$NAMELHEMODIFIED
+echo ">>> prepare local folder"
+cd /tmp/
+TESTFOLDER="testDecay_"$ENERGY"_"$NUMBERSEED"_"$CSI
+MODELFOLDER="My"$MODEL"_"$CSI
+MODELFOLDERTGZ=$MODELFOLDER"_"$NUMBERSEED"_"$ENERGY".tgz"
+# MODELFOLDERTGZ="My"$MODEL"_"$CSI"_"$NUMBERSEED"_"$ENERGY".tgz"
 
-### decay the Higgs and hadronize
+mkdir /tmp/$TESTFOLDER
+
+
+echo ">>> copy from castor"
+rfcp $CASTORFOLDERNAME/$MODELFOLDERTGZ ./$TESTFOLDER
+
+
+echo ">>> untar model"
+cd /tmp/$TESTFOLDER
+tar -xf $MODELFOLDERTGZ
+
+
+echo ">>> go to Events folder and gunzip"
+ls
+cd $MODELFOLDER
+cd Events
+ls
+
+# MODEL="SO5" ---> fast fix due to bug! 0 <-> O
+
+NAMEFILELHEGZ=$MODEL"at"$ENERGY"TeV_unweighted_events.lhe.gz"
+gzip -d $NAMEFILELHEGZ
+ls
+
+NAMEFILELHE=$MODEL"at"$ENERGY"TeV_unweighted_events.lhe"
+
+
+echo ">>>  modify H -> 135, to make it properly decay"
+
+NAMELHEMODIFIED=$MODEL"at"$ENERGY"TeV_unweighted_events_modified.lhe"
+awk '/\ \ 25\ \ /&&v++%2{sub(/\ \ 25\ \ \ \ 1/, "\ \ 35\ \ \ \ 1")}{print}' $NAMEFILELHE  >  /tmp/$TESTFOLDER/$NAMELHEMODIFIED
+
+
+echo ">>>  decay the Higgs and hadronize"
+head  /tmp/$TESTFOLDER/$NAMELHEMODIFIED
+echo "..."
+tail  /tmp/$TESTFOLDER/$NAMELHEMODIFIED
+
 cd /afs/cern.ch/work/a/amassiro/Generation/CMSSW_5_3_9_patch1/src/
 cmsenv
 eval `scramv1 runtime -sh`
 
-# prepare input
-cmsDriver.py MCDBtoEDM --conditions START53_V7C::All -s NONE --eventcontent RAWSIM --datatier GEN --filein file:/tmp/amassiro/$NAMELHEMODIFIED   --fileout /tmp/amassiro/$NAMEEDM  -n -1
 
-# prepare cfg file for decay
+echo ">>>  prepare input"
+cmsDriver.py MCDBtoEDM --conditions START53_V7C::All -s NONE --eventcontent RAWSIM --datatier GEN --filein file:/tmp/$TESTFOLDER/$NAMELHEMODIFIED   --fileout /tmp/$TESTFOLDER/$NAMEEDM  -n -1
 
-
-
-### decay and hadronize
+echo ">>>  prepare cfg file for decay"
 
 CFGFILE="POWHEG_PYTHIA6_Tauola_HH_bbWW_lnulnu_withTau_TEMPLATETeV_cff_py_GEN.py"
-NEWCFGFILE="POWHEG_PYTHIA6_Tauola_HH_bbWW_lnulnu_withTau_"$ENERGY"_TeV_"$MODEL"_"$NUMBERSEED"_"$FOLDERNAME"_cff_py_GEN.py"
+NEWCFGFILE="POWHEG_PYTHIA6_Tauola_HH_bbWW_lnulnu_withTau_"$ENERGY"_TeV_"$MODEL"_"$NUMBERSEED"_"$CSI"_cff_py_GEN.py"
 
 
-cat $CFGFILE | sed -e s%TEMPLATEENERGY%$ENERGY%g | sed -e s%TEMPLATEINPUTFILE%/tmp/amassiro/$NAMEEDM%g | sed -e s%TEMPLATEOUTPUTFILE%/tmp/amassiro/$NAMEGEN%g > /tmp/amassiro/$NEWCFGFILE
+cat $CFGFILE | sed -e s%TEMPLATEENERGY%$ENERGY%g | sed -e s%TEMPLATEINPUTFILE%/tmp/$TESTFOLDER/$NAMEEDM%g | sed -e s%TEMPLATEOUTPUTFILE%/tmp/$TESTFOLDER/$NAMEGEN%g > /tmp/$TESTFOLDER/$NEWCFGFILE
 
-## actually decaying
-cmsRun $CFGFILE
+cat /tmp/$TESTFOLDER/$NEWCFGFILE
 
-## copy result
-cp /tmp/amassiro/$NAMEGEN /afs/cern.ch/work/a/amassiro/Generation/HH/DATA/
+echo ">>> decay and hadronize"
 
-
+cmsRun /tmp/$TESTFOLDER/$NEWCFGFILE
 
 
+echo ">>> copy result"
+# cp /tmp/amassiro/$NAMEGEN /afs/cern.ch/work/a/amassiro/Generation/HH/DATA/
+# cp /tmp/amassiro/$NAMEGEN /afs/cern.ch/user/a/amassiro/DATA/
+# eoscms cp /tmp/amassiro/$NAMEGEN   /store/caf/user/amassiro/TEST/
+rfcp   /tmp/$TESTFOLDER/$NAMEGEN   /castor/cern.ch/user/a/amassiro/HH-WWbb
 
+echo ">>> clean up a little"
+rm -r /tmp/$TESTFOLDER/
 
---
+++ SubmitDecayAndHadronize.py
@@ -0,0 +1,40 @@
+#!/bin/env python
+
+import os
+import subprocess
+import sys
+
+max    = int(sys.argv[1])
+folder = sys.argv[2]
+model  = sys.argv[3]
+energy = sys.argv[4]
+csi    = sys.argv[5]
+
+wd     = os.getcwd()
+queue  = '8nh'
+#queue  = '1nh' # -> not enough !?!?
+#queue  = '8nm' #--> not enough time!
+newfol = 'makeDecay_{ENERGY}'.format(ENERGY=energy)
+os.system('mkdir -p {PWD}/{NEW}'.format(PWD=wd,NEW=newfol))
+
+
+
+if len(sys.argv) == 1:
+   print "uff, not enough information"
+   sys.exit(0)
+
+for i in range(max) :
+  command = '#!/bin/bash\n\
+              cd {PWD}\n\
+              ./DecayAndHadronize.sh {ITER} {FOLDER} {MODEL} {ENERGY} {CSI} \n \
+             '.format(PWD=wd, ITER=str(i), FOLDER=folder, MODEL=model, ENERGY=energy, CSI=csi).replace('  ','')
+
+  fname = '/'.join([wd,newfol,'sub_'+str(i)+'_'+model+'_'+energy+'_'+csi+'.sh'])
+  f1    = open(fname, 'w+')
+  print >> f1 , command
+  os.system('chmod 755 {FILE}'.format(FILE=fname)) 
+  f1.close()
+
+for i in range(max) :
+  fname = '/'.join([wd,newfol,'sub_'+str(i)+'_'+model+'_'+energy+'_'+csi+'.sh'])
+  subprocess.Popen(['bsub -q {QUEUE} < {FILE}'.format(QUEUE=queue,FILE=fname)], stdout=subprocess.PIPE, shell=True)
